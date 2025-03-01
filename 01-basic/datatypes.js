79b3dff619329645298b95bc352b059c08c9afc1
srishtidev25 <Srishtidev25@gmail.com>
2025-03-01 10:38:17 +0000

    datatypes

---
+++ 01-basic/datatypes.js
@@ -0,0 +1,10 @@
+"use strict";  // treat all the code as newer version
+
+let name = "srishti"
+let age = 18
+let isLoggedIn = false
+
+
+console.log(typeof "srishti");
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefiend
---
+++ 01-basic/variables.js
@@ -0,0 +1,20 @@
+const accountID = 24563
+let accountEmail = "sri@gmail.com"
+var accountPassword = "6576"
+accountCity = "jaipur"
+
+// accountID = 2 //
+
+// console.log(accountID); //
+
+/*
+prefer not to use var beacause block scope and functional scope
+*/
+
+accountEmail = "sdev@gmail.com"
+accountPassword = "54"
+accountCity = "dehradun"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
